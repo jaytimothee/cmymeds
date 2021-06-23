02fb2b44065ca345d32c18370f7b49f2fb516c34
job timothee <timotheej@outlook.com>
2021-06-23 14:03:27 -0400

    changes

--- src/SQL/query.sql
+++ src/SQL/query.sql
@@ -11,9 +11,25 @@ SELECT mtm_credits.person_id, people.name, roles.role
 FROM mtm_credits INNER JOIN people ON mtm_credits.person_id = people.id
 INNER JOIN roles ON people.id = roles.id;
 
+
+--Which director has directed the most movies in our database?
+SELECT movies.id, mtm_credits.person_id, people.name
+FROM mtm_credits JOIN  movies ON mtm_credits.movie_id = movies.id
+JOIN people ON mtm_credits.person_id = people.id
+GROUP BY movies.id, people.name, mtm_credits.person_id
+ORDER BY COUNT(*) DESC LIMIT 1;
+
 --List all of the people who have directed a movie.
 SELECT people.name, roles.role
 FROM mtm_credits JOIN people ON mtm_credits.person_id = people.id
 JOIN roles ON mtm_credits.role_id = roles.id;
+
+
+
+--List all of the people who have worked with Ben Affleck
+SELECT mtm_credits.person_id, people.name, roles.id
+FROM people JOIN mtm_credits ON mtm_credits.person_id = people.id
+JOIN roles ON people.id = roles.id;
+
 
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,39 +1,57 @@
-const fs = require('fs')
+const fs = require('fs') // node js file system required to perfom file opperations
 
+//Read in file from path
 fs.readFile('./file/word_wrap.txt', 'utf8', (err, data) => {
+    //exit early if error
     if (err) {
         return console.log(err)
     }
-    let start = 0;
-    let wrap = 80;
 
+    let start = 0; //variable to start at begining of file
+    let wrap = 80; //number of characters to wrap
+
+
+    //[start itterating file]
+
+    // spread file data into [] tobe able to use array methods like forEach
     [...data].forEach((char, index, arry) => {
 
-
+        // loop over text and get the first 80 characters from start positions (start = x)
         let text = arry.slice(start, wrapFunction(wrap, arry));
 
+        //append text into a file and insert new line char
         fs.appendFile('./test.txt', text.join('') + '\n', function (err) {
             if (err) throw err;
 
         });
-
+        //update start position and wrap counter to default value
         start += 80;
         wrap += 80;
 
     })
 
+    //[end itteration]
+
+
+    /**
+     * Using reccursion to call a function that will calculate new wrap index and start position
+     * @param {int} wrap 
+     * @param {Array} arry 
+     * @returns {int} calculate wrap index
+     */
     function wrapFunction(wrap, arry) {
-        if (wrap >= arry.length || start >= arry.length){
+        //check if wrap index || start is at end of file 
+        if (wrap >= arry.length || start >= arry.length) {
             --wrap;
             --start
         }
-
+        // check if wrap index is equal to whitespace 
         if (arry[wrap] === ' ') {
-            wrap--;
+            --wrap; //decrement wrap and so that the word is not broken call function again
             wrapFunction(wrap, arry)
 
         } else {
-            return wrap + 1
+            return wrap + 1 // return wrap index 
         }
     }
 })
